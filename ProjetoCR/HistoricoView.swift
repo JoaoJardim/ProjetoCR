17244ae9861eaaa1c59648e4911ff570628c1847
Gabrielle Brandenburg dos Anjos <gabriellebrandenburg@gmail.com>
2016-03-30 17:25:34 -0300

    view classes, controller classes, views da tela de login

---
+++ ProjetoCR/HistoricoView.swift
@@ -0,0 +1,21 @@
+//
+//  HistoricoView.swift
+//  ProjetoCR
+//
+//  Created by Gabrielle Brandenburg dos Anjos on 3/30/16.
+//  Copyright © 2016 João Gabriel Jardim. All rights reserved.
+//
+
+import UIKit
+
+class HistoricoView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
